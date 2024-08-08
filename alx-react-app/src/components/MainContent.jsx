b36791f39f79184eb45d278a8c38c76341eb26ec
Yosef Fekade <155354713+YosefFekade@users.noreply.github.com>
2024-08-08 12:54:49 +0300

    Update MainContent.jsx

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,3 +1,11 @@
-<main>
-  <p>I love to visit New York, Paris, and Tokyo.</p>
-</main>
+function MainContent() {
+    return (
+        <div>
+            <main>
+             <p>I love to visit New York, Paris, and Tokyo.</p>
+            </main>
+        </div>
+    );
+}
+
+export default MainContent;
