e9afd072aea1206b28cff8277f3a8be10327acfc
Yosef Fekade <155354713+YosefFekade@users.noreply.github.com>
2024-08-08 12:38:39 +0300

    Create WelcomeMessage.jsx

---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,10 @@
+function WelcomeMessage() {
+    return (
+        <div>
+            <h1>Welcome to ALX React APP!</h1>
+            <p>This is a simple JSX component.</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage;
