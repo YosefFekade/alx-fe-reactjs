3806653f097fa6b31c5c6ef3b1b8adb0a41c5db4
Yosef Fekade <155354713+YosefFekade@users.noreply.github.com>
2024-08-08 12:55:57 +0300

    Update Footer.jsx

--- alx-react-app/src/components/Footer.jsx
+++ alx-react-app/src/components/Footer.jsx
@@ -1,3 +1,11 @@
-<footer>
-  <p>© 2023 City Lovers</p>
-</footer>
+function Footer() {
+    return (
+        <div>
+            <footer>
+              <p>© 2023 City Lovers</p>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
