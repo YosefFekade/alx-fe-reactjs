d04fe736249edbb2286cc48126ee2578ba31b80c
Yosef Fekade <155354713+YosefFekade@users.noreply.github.com>
2024-08-08 12:41:10 +0300

    Update WelcomeMessage.jsx

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,8 +1,9 @@
 function WelcomeMessage() {
     return (
         <div>
-            <h1>Welcome to ALX React APP!</h1>
+            <h1> “Hello everyone, I am learning React at ALX!”</h1>
             <p>This is a simple JSX component.</p>
+            <p>"I am learning about JSX!".</p>
         </div>
     );
 }
