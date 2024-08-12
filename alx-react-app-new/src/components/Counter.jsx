356efab7736e5a61c6bd35e3d99e57ef77f31c32
Yosef Fekade <155354713+YosefFekade@users.noreply.github.com>
2024-08-12 12:02:41 +0300

    Update Counter.jsx

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -1,1 +1,15 @@
+import { useState } from 'react';
 
+function Counter() {
+     const [count, setCount] = useState(0);
+
+     return (
+       <div>
+         <p>Current Count: {count}</p>
+         <button onClick={() => setCount(count + 1)}>Increment</button>
+         <button onClick={() => setCount(count - 1)}>Decrement</button>
+         <button onClick={() => setCount(0)}>Reset</button>
+       </div>
+     );
+   }
+export default Counter;
