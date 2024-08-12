606cd6f7701a52e70566f437ebf708f68f29606c
Yosef Fekade <155354713+YosefFekade@users.noreply.github.com>
2024-08-12 12:03:47 +0300

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -5,6 +5,7 @@ import Header from './components//Header';
 import MainContent from './components//MainContent';
 import Footer from './components//Footer';
 import UserProfile from './components//UserProfile';
+import Counter from './components//Counter';
 
 function App() {
   // const [count, setCount] = useState(0)
