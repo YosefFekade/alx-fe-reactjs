e6fb5b9580feda0b139b220fe9cf73f8880d1337
Yosef Fekade <155354713+YosefFekade@users.noreply.github.com>
2024-08-12 12:38:48 +0300

    Update App.jsx

--- alx-react-app-props/src/components/App.jsx
+++ alx-react-app-props/src/components/App.jsx
@@ -1,9 +1,15 @@
+import React from 'react';
 import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  return <ProfilePage userData={userData} />;
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
 }
 
 export default App;
