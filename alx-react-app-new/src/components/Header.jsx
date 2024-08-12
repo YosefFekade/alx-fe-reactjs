cb162f04102e8f338b2a8f1f8ee543255d239f36
Yosef Fekade <155354713+YosefFekade@users.noreply.github.com>
2024-08-12 11:12:41 +0300

    Update Header.jsx

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -2,9 +2,9 @@ import React from "react";
 function Header() {
     return (
         <div>
-            <header>
-              <h1>My Favorite Cities</h1>
-           </header>
+            <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+                      <h1>My Favorite Cities</h1>
+            </header>
         </div>
     );
 }
