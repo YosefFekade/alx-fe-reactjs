2f1b1e3955959c529c8b329115da63d6356d5b6d
Yosef Fekade <155354713+YosefFekade@users.noreply.github.com>
2024-08-12 12:26:10 +0300

    Create UserDetails.jsx

---
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -0,0 +1,10 @@
+function UserDetails({ userData }) {
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserDetails;
