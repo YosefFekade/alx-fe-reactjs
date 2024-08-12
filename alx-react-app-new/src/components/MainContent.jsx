f04bd5f73c31c87ce65e7d94b4e9096018dfe482
Yosef Fekade <155354713+YosefFekade@users.noreply.github.com>
2024-08-12 11:19:42 +0300

    Update MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,8 +2,8 @@ import React from "react";
 function MainContent() {
     return (
         <div>
-            <main>
-             <p>I love to visit New York, Paris, and Tokyo.</p>
+            <main  style={{ backgroundColor: 'gray', color: 'black', textAlign: 'center' }}>
+             <p style={{color='navy'}}>I love to visit New York, Paris, and Tokyo.</p>
             </main>
         </div>
     );
