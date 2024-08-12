00b9b426564edb3902a66d5677b22e61beac0dc5
Yosef Fekade <155354713+YosefFekade@users.noreply.github.com>
2024-08-12 12:14:58 +0300

    Add files via upload

---
+++ alx-react-app-props/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function MainContent() {
+    return (
+        <div>
+            <main>
+             <p>I love to visit New York, Paris, and Tokyo.</p>
+            </main>
+        </div>
+    );
+}
+
+export default MainContent;
---
+++ alx-react-app-props/src/components/WelcomeMessage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function WelcomeMessage() {
+    return (
+        <div>
+            <h1> “Hello everyone, I am learning React at ALX!”</h1>
+            <p>This is a simple JSX component.</p>
+            <p>"I am learning about JSX!".</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage;
