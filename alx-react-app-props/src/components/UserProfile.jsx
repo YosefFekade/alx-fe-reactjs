04870fbd10dcc57f8cd2cd294fe6cf1a77ba2bea
Yosef Fekade <155354713+YosefFekade@users.noreply.github.com>
2024-08-13 09:38:49 +0300

    Update UserProfile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -5,9 +5,9 @@ const UserProfile = (props) => {
    return (
      <div>
          useContext
-          <h2>{props.UserContext}</h2>
-          <p>Age: {props.UserContext}</p>
-          <p>Bio: {props.UserContext}</p>
+          <h2>{props.name}</h2>
+          <p>Age: {props.age}</p>
+          <p>Bio: {props.bio}</p>
      
      </div>
    );
