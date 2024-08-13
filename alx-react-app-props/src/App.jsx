a58d8c125627914ce1ffbeab036b1d740e983377
Yosef Fekade <155354713+YosefFekade@users.noreply.github.com>
2024-08-13 09:30:16 +0300

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -5,9 +5,23 @@ import Header from './components//Header';
 import MainContent from './components//MainContent';
 import Footer from './components//Footer';
 import UserProfile from './components//UserProfile';
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
+
 
 function App() {
-  // const [count, setCount] = useState(0)
+ 
+
+const UserContext = createContext(null);
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+ 
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+
 
   return (
    <div>
@@ -19,26 +33,5 @@ function App() {
    </div>
   )
 }
- // <>
-    //   <div>
-    //     <a href="https://vitejs.dev" target="_blank">
-    //       <img src={viteLogo} className="logo" alt="Vite logo" />
-    //     </a>
-    //     <a href="https://react.dev" target="_blank">
-    //       <img src={reactLogo} className="logo react" alt="React logo" />
-    //     </a>
-    //   </div>
-    //   <h1>Vite + React</h1>
-    //   <div className="card">
-    //     <button onClick={() => setCount((count) => count + 1)}>
-    //       count is {count}
-    //     </button>
-    //     <p>
-    //       Edit <code>src/App.jsx</code> and save to test HMR
-    //     </p>
-    //   </div>
-    //   <p className="read-the-docs">
-    //     Click on the Vite and React logos to learn more
-    //   </p>
-    // </>
+
 export default App
