094e81c46777645f0acb2756865098ddab007fd8
Yosef Fekade <155354713+YosefFekade@users.noreply.github.com>
2024-08-16 17:47:43 +0300

    Add files via upload

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav style={{ padding: '10px', background: '#333', color: '#fff' }}>
+      <Link to="/" style={{ margin: '0 10px', color: '#fff' }}>Home</Link>
+      <Link to="/about" style={{ margin: '0 10px', color: '#fff' }}>About</Link>
+      <Link to="/services" style={{ margin: '0 10px', color: '#fff' }}>Services</Link>
+      <Link to="/contact" style={{ margin: '0 10px', color: '#fff' }}>Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
