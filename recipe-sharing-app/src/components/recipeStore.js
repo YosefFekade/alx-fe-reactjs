1e72fc6c0406ca61b4654945dd88c8535a269617
Yosef Fekade <155354713+YosefFekade@users.noreply.github.com>
2024-08-22 18:49:44 +0300

    Create recipeStore.js

---
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -0,0 +1,7 @@
+import create from 'zustand'
+
+const useRecipeStore = create(set => ({
+  recipes: [],
+  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes })
+}));
