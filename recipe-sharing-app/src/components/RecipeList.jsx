616ec36104eae0820927a7da73a258b82cf28d2d
Yosef Fekade <155354713+YosefFekade@users.noreply.github.com>
2024-08-23 09:44:01 +0300

    Update RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,9 +1,6 @@
-<<<<<<< HEAD:recipe-sharing-app/src/Components/RecipeList.jsx
+
 import { Link } from 'react-router-dom';
 import { useRecipeStore } from '../recipeStore';
-=======
-import { useRecipeStore } from './recipeStore';
->>>>>>> 436dc88c6ff65c3a725d2e66edbe3690f7041bf9:recipe-sharing-app/src/components/RecipeList.jsx
 
 const RecipeList = () => {
   const recipes = useRecipeStore(state => state.recipes);
@@ -20,9 +17,5 @@ const RecipeList = () => {
     </div>
   );
 };
-<<<<<<< HEAD:recipe-sharing-app/src/Components/RecipeList.jsx
 
 export default RecipeList;
-=======
-export default RecipeList
->>>>>>> 436dc88c6ff65c3a725d2e66edbe3690f7041bf9:recipe-sharing-app/src/components/RecipeList.jsx
