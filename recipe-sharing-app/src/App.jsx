37577258b533591d36c334fd1862318efe15e566
Yosef Fekade <155354713+YosefFekade@users.noreply.github.com>
2024-08-23 11:19:49 +0300

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RecipeList from '../components/RecipeList';
 import AddRecipeForm from '../components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
+import SearchBar from './components/SearchBar';
+import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList';
 
 const App = () => {
   return (
@@ -10,8 +13,11 @@ const App = () => {
         <Route path="/" element={
           <div>
             <h1>Recipe Sharing App</h1>
+            <SearchBar />
             <AddRecipeForm />
             <RecipeList />
+            <FavoritesList />
+            <RecommendationsList />
           </div>
         } />
         <Route path="/recipe/:id" element={<RecipeDetails />} />
