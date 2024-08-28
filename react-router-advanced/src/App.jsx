3cf6299d6b78afca0a125e51720e3227e84362c7
Yosef Fekade <155354713+YosefFekade@users.noreply.github.com>
2024-08-28 10:46:27 +0300

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -4,15 +4,16 @@ import Home from './components/Home';
 import Profile from './components/Profile';
 import BlogPost from './components/BlogPost';
 import ProtectedRoute from './components/ProtectedRoute'; 
-
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
+          {/* Protected Route for Profile */}
           <Route path="/profile/*" element={<ProtectedRoute component={Profile} />} />
-          <Route path="/post/:id" element={<BlogPost />} />
+          {/* Dynamic Route for Blog Post */}
+          <Route path="/blog/:id" element={<BlogPost />} />
         </Routes>
       </div>
     </Router>
