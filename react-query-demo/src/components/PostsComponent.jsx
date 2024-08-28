e52905dedb7b06d616721daad312c8c8cbaa6da6
Yosef Fekade <155354713+YosefFekade@users.noreply.github.com>
2024-08-28 10:21:11 +0300

    Update PostsComponent.jsx

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -12,10 +12,10 @@ const fetchPosts = async () => {
 
 const PostsComponent = () => {
  
-  const { data, error, isLoading, refetch } = useQuery('posts', fetchPosts);
+  const { data, isError, error, isLoading, refetch } = useQuery('posts', fetchPosts);
 
   if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
+  if (isError) return <div>Error: {error.message}</div>;
 
   return (
     <div>
