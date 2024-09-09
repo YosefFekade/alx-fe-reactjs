8d59fdcd6f2872904335cd45ae42a70ada2d7076
Yosef <admaseyohanes@gmail.com>
2024-09-09 09:50:11 +0300

    first p\step completed

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-blue-500'>THis is a test</h1>
+    </>
+  )
+}
+
+export default App
