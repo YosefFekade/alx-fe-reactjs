84a7efd012ccc84b2780828320ad27b7d3d0878e
Yosef <admaseyohanes@gmail.com>
2024-09-19 12:17:04 +0300

    again solved error

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api.github.com/search/users';
+const BASE_URL = 'https://api.github.com/search/users?q';
 
 /**
  * Fetch GitHub users based on search criteria.
