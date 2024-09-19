42b07e9d68b7fe3c5b17861fb99024136f1e4c77
Yosef <admaseyohanes@gmail.com>
2024-09-19 10:38:56 +0300

    fixed bug again

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -16,7 +16,7 @@ const Search = () => {
       const response = await axios.get(`https://api.github.com/users/${username}`);
       setUser(response.data);
     } catch (err) {
-      setError('Looks like we can’t find the user');
+      setError("Looks like we cant find the user");
     } finally {
       setLoading(false);
     }
